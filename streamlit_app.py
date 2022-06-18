565c1b278e5731ac6d90596591746c0d74fdfd02
Vasanthi Chandiran <107747587+vasanthichandiran@users.noreply.github.com>
2022-06-18 14:19:08 +0530

    Update streamlit_app.py
    copy pasted emojis

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 import streamlit
 streamlit.title('My Parents New Healthy Diner')
 streamlit.header('Breakfast Menu')
-streamlit.text('Omega 3 & Buleberry Oatmeal')
-streamlit.text('Kalem Spinach & Rocket Smoothie')
-streamlit.text(:poultry_leg:'Hard-Boiled Free-Range Egg')
-streamlit.text('Avocado Toast')
+streamlit.text('🥣 Omega 3 & Buleberry Oatmeal')
+streamlit.text('🥗 Kalem Spinach & Rocket Smoothie')
+streamlit.text('🐔 Hard-Boiled Free-Range Egg')
+streamlit.text('🥑🍞 Avocado Toast')
