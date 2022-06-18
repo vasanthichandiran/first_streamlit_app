a073d8aaf208db410582179f250ab2af534f931a
Vasanthi Chandiran <107747587+vasanthichandiran@users.noreply.github.com>
2022-06-18 14:06:43 +0530

    Update streamlit_app.py
    modified streamlit.write to steamlit.text

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,8 +4,8 @@ streamlit.title('My Parents New Healthy Diner')
 
 streamlit.header('Breakfast Menu')
 
-streamlit.write('Omega 3 & Buleberry Oatmeal')
+streamlit.text('Omega 3 & Buleberry Oatmeal')
 
-streamlit.write('Kalem Spinach & Rocket Smoothie')
+streamlit.text('Kalem Spinach & Rocket Smoothie')
 
-streamlit.write('Hard-Boiled Free-Range Egg')
+streamlit.text('Hard-Boiled Free-Range Egg')
