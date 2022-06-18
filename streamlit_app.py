7127da5c956a87963ac15359af28f36ee4118811
Vasanthi Chandiran <107747587+vasanthichandiran@users.noreply.github.com>
2022-06-18 14:16:01 +0530

    Update streamlit_app.py
    added :poultry_leg:

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,11 +1,7 @@
 import streamlit
-
 streamlit.title('My Parents New Healthy Diner')
-
 streamlit.header('Breakfast Menu')
-
 streamlit.text('Omega 3 & Buleberry Oatmeal')
-
 streamlit.text('Kalem Spinach & Rocket Smoothie')
-
-streamlit.text('Hard-Boiled Free-Range Egg')
+streamlit.text(:poultry_leg:'Hard-Boiled Free-Range Egg')
+streamlit.text('Avocado Toast')
